a92d4a624d9b8da34f69bce144e90ed5b2d03d21
coolbob1998 <coolbob1998@qq.com>
2022-09-21 16:18:18 +0800

    feat: 修改测试用例

--- packages/core/src/__tests__/fl.spec.ts
+++ packages/core/src/__tests__/fl.spec.ts
@@ -1,50 +1,43 @@
-import { autorun } from '@formily/reactive'
-import { createForm, onFieldReact } from '..'
-import { attach, sleep } from './shared'
+import { createForm } from '..'
+import { attach } from './shared'
 
-test('field linkage', () => {
-  const handler1 = jest.fn()
-  const handler2 = jest.fn()
-  const form = attach(
-    createForm({
-      effects() {
-        onFieldReact('field2')
-      },
-    })
-  )
-
-  const layoutField = attach(form.createVoidField({ name: 'layout' }))
-
+test('child field reactions', () => {
+  const form = attach(createForm())
+  const voidField = attach(form.createVoidField({ name: 'void' }))
   const field1 = attach(
     form.createField({
       name: 'field1',
-      basePath: layoutField.address,
+      basePath: voidField.address,
+      reactions: [
+        (field) => {
+          field.value = field.query('field3').getIn('value')
+        },
+      ],
     })
   )
-  expect(handler1).toBeCalledTimes(0)
-  expect(handler2).toBeCalledTimes(0)
-  autorun(() => {
-    const field2Value = field1.query('field2').getIn('value')
-    handler1()
-  })
-  autorun(() => {
-    const field3Value = field1.query('.field3').getIn('value')
-    handler2()
-  })
-  expect(handler1).toBeCalledTimes(1)
-  expect(handler2).toBeCalledTimes(1)
   const field2 = attach(
     form.createField({
       name: 'field2',
-      basePath: layoutField.address,
+      basePath: voidField.address,
+      reactions: [
+        (field) => {
+          field.value = field.query('.field3').getIn('value')
+        },
+      ],
     })
   )
+  expect(field1.value).toBeUndefined()
+  expect(field2.value).toBeUndefined()
   const field3 = attach(
     form.createField({
       name: 'field3',
-      basePath: 'layout',
+      basePath: voidField.address,
+      value: 1,
     })
   )
-  expect(handler1).toBeCalledTimes(2)
-  expect(handler2).toBeCalledTimes(2)
+  expect(field1.value).toBe(1)
+  expect(field2.value).toBe(1)
+  field3.value = 2
+  expect(field1.value).toBe(2)
+  expect(field2.value).toBe(2)
 })
